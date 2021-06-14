bf5a79dc06d37105c512845412fa4e067e3649da
Georgi Gerganov <ggerganov@gmail.com>
2021-06-14 20:56:41 +0300

    wasm : disable various audio manipulations in the browser
    - echo cancellation
    - auto gain control
    - noise suppression

---
+++ examples/ggmorse-gui/gghelpers.js
@@ -0,0 +1,108 @@
+function checkSharedArrayBuffer() {
+    try {
+        var a = SharedArrayBuffer;
+    } catch (e) {
+        console.log(e instanceof ReferenceError); // true
+        return false;
+    }
+
+    return true;
+}
+
+function mobileCheck() {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+
+function adjustAudioConstraints() {
+    function setLegacyChromeConstraint(constraint, name, value) {
+        if (constraint.mandatory && name in constraint.mandatory) {
+            constraint.mandatory[name] = value;
+            return;
+        }
+        if (constraint.optional) {
+            const element = constraint.optional.find(opt => name in opt);
+            if (element) {
+                element[name] = value;
+                return;
+            }
+        }
+        // `mandatory` options throw errors for unknown keys, so avoid that by
+        // setting it under optional.
+        if (!constraint.optional) {
+            constraint.optional = [];
+        }
+        constraint.optional.push({ [name]: value });
+    }
+
+    function setConstraint(constraint, name, value) {
+        if (constraint.advanced) {
+            const element = constraint.advanced.find(opt => name in opt);
+            if (element) {
+                element[name] = value;
+                return;
+            }
+        }
+        constraint[name] = value;
+    }
+
+    function disableAll(constraints) {
+        console.log("Disabling all audio constraints:");
+        console.log(" - echo cancellation");
+        console.log(" - auto gain control");
+        console.log(" - noise suppression");
+        console.log(" - highpass filter ?");
+        console.log(" - typing noise detection ?");
+        if (constraints && constraints.audio) {
+            if (typeof constraints.audio !== "object") {
+                constraints.audio = {};
+            }
+            if (constraints.audio.optional || constraints.audio.mandatory) {
+                setLegacyChromeConstraint(constraints.audio, "googEchoCancellation", false);
+                setLegacyChromeConstraint(constraints.audio, "googAutoGainControl", false);
+                setLegacyChromeConstraint(constraints.audio, "googAutoGainControl2", false);
+                setLegacyChromeConstraint(constraints.audio, "googNoiseSuppression", false);
+                setLegacyChromeConstraint(constraints.audio, "googHighpassFilter", false);
+                setLegacyChromeConstraint(constraints.audio, "googTypingNoiseDetection", false);
+            } else {
+                setConstraint(constraints.audio, "echoCancellation", false);
+                setConstraint(constraints.audio, "autoGainControl", false);
+                setConstraint(constraints.audio, "noiseSuppression", false);
+                setConstraint(constraints.audio, "highpassFilter", false); // not sure if this does anything
+                setConstraint(constraints.audio, "typingNoiseDetection", false); // not sure if this does anything
+            }
+        }
+    }
+
+    function patchFunction(object, name, createNewFunction) {
+        if (name in object) {
+            var original = object[name];
+            object[name] = createNewFunction(original);
+        }
+    }
+
+    patchFunction(navigator.mediaDevices, "getUserMedia", function (original) {
+        return function getUserMedia(constraints) {
+            disableAll(constraints);
+            return original.call(this, constraints);
+        };
+    });
+
+    function patchDeprecatedGetUserMedia(original) {
+        return function getUserMedia(constraints, success, error) {
+            disableAll(constraints);
+            return original.call(this, constraints, success, error);
+        };
+    }
+
+    patchFunction(navigator, "getUserMedia", patchDeprecatedGetUserMedia);
+    patchFunction(navigator, "mozGetUserMedia", patchDeprecatedGetUserMedia);
+    patchFunction(navigator, "webkitGetUserMedia", patchDeprecatedGetUserMedia);
+    patchFunction(MediaStreamTrack.prototype, "applyConstraints", function (original) {
+        return function applyConstraints(constraints) {
+            disableAll(constraints);
+            return original.call(this, constraints);
+        };
+    });
+};
